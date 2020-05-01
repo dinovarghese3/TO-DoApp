681d2b3cfb2d56d4723174a261fa23e011932dca
unknown <dinovarghese3@gmail.com>
2020-05-01 10:57:21 +0530

    Updates

---
+++ precache-manifest.8a000ce793d8075f605aa98111303533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918413f0dde61d3541cb05dab07ebd40",
+    "url": "/TO-DoApp.gihub.io/index.html"
+  },
+  {
+    "revision": "6bc24f4783e6ee6e786a",
+    "url": "/TO-DoApp.gihub.io/static/css/main.6b451783.chunk.css"
+  },
+  {
+    "revision": "72ca57b74343ca27caad",
+    "url": "/TO-DoApp.gihub.io/static/js/2.f514f56a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO-DoApp.gihub.io/static/js/2.f514f56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc24f4783e6ee6e786a",
+    "url": "/TO-DoApp.gihub.io/static/js/main.99c89376.chunk.js"
+  },
+  {
+    "revision": "d40e9389da0467a71315",
+    "url": "/TO-DoApp.gihub.io/static/js/runtime-main.b3b04046.js"
+  }
+]);
